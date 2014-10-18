5d973ecbc485428a73bd86fd7eca87fac314281b
dDua <dheeru_dua@hotmail.com>
2014-10-18 00:32:16 -0400

    updated

--- QryEval.java
+++ QryEval.java
@@ -181,12 +181,12 @@ public int compare(scoreArray o1,
 			ret = new RetrievalModelRankedBoolean();
 		}
 		
-		//Qryop parsedQuery1 = parseQuery("#WINDOW/8(cheap internet)",ret);
+		Qryop parsedQuery1 = parseQuery("#WINDOW/8(heart rate)",ret);
 		//Qryop parsedQuery1 = parseQuery("brooks brothers clearance",ret);
 		//if(ret instanceof RetreivalModelIndri){
 			//parsedQuery1 = multipleRep(parsedQuery1);
 		//}
-		//QryResult result1 = parsedQuery1.evaluate(ret);
+		QryResult result1 = parsedQuery1.evaluate(ret);
 		//int id1 = getInternalDocid("clueweb09-en0001-66-14262");
 		//int id2 = getInternalDocid("clueweb09-en0011-58-19607");
 		BufferedReader reader = null;
@@ -380,12 +380,13 @@ public int compare(scoreArray o1,
 	    	hashes +=1;
 	    }
 	    
-	    if(qString.toLowerCase().startsWith("#window") || qString.toLowerCase().startsWith("#near")){
+	    
+		if(!qString.startsWith("(")){
+			qString = defaultOperator.toString().replace("(","").replace(")", "").trim()+"(" + qString + ")";
+		}
+		if(qString.toLowerCase().startsWith("#window") || qString.toLowerCase().startsWith("#near")){
 	    	qString = defaultOperator.toString().replace("(","").replace(")", "").trim()+"(" + qString + ")";
 	    }
-		if(!qString.startsWith("(") && hashes!=1 ){
-			qString = defaultOperator.toString() +"(" + qString + ")";
-		}
 //		if (qString.charAt(0) != '#' && r instanceof RetrievalModelTfidfRanked) {
 //				qString = "#or(" + qString + ")";
 //				//stack.push(new QryopSlOR());
